66083541d1f40e472a5ed560b65600fad69ff453
Babla <bablas@gmail.com>
2019-04-02 18:11:15 -0400

    Update sakila-analysis.sql

--- sakila-analysis.sql
+++ sakila-analysis.sql
@@ -1,95 +1,79 @@
 USE sakila;
 
--- 1a. 
+
 
 SELECT first_name, last_name
 FROM actor;
 
--- 1b.  
 
 SELECT UPPER(CONCAT(first_name, ' ', last_name)) AS `Actor Name`
 FROM actor;
 
--- 2a.  
 
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE first_name = "Joe";
 
--- 2b. 
 
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE last_name like '%GEN%';
 
--- 2c.
 
 SELECT actor_id, last_name, first_name
 FROM actor
 WHERE last_name like '%LI%';
 
--- 2d. 
 
 SELECT country_id, country
 FROM country 
 WHERE country IN 
 ('Afghanistan', 'Bangladesh', 'China');
 
--- 3a.
 
 ALTER TABLE actor
 ADD COLUMN middle_name VARCHAR(25) AFTER first_name;
 
--- 3b.
 
 ALTER TABLE actor
 MODIFY COLUMN middle_name BLOB;
 
--- 3c.
 
 ALTER TABLE actor
 DROP COLUMN middle_name;
 
--- * 4a.
 
 SELECT last_name, COUNT(*) AS 'Number of Actors' 
 FROM actor GROUP BY last_name;
 
--- 4b. 
 
 SELECT last_name, COUNT(*) AS 'Number of Actors' 
 FROM actor GROUP BY last_name HAVING count(*) >=2;
 
--- 4c. 
 
 UPDATE actor 
 SET first_name = 'HARPO'
 WHERE First_name = "Groucho" AND last_name = "Williams";
 
--- 4d.
 
 UPDATE actor 
 SET first_name = 'GROUCHO'
 WHERE actor_id = 172;
 
--- 5a. 
 
 DESCRIBE sakila.address;
 
--- 6a. 
 
 SELECT first_name, last_name, address
 FROM staff s 
 JOIN address a
 ON s.address_id = a.address_id;
 
--- 6b. 
 
 SELECT payment.staff_id, staff.first_name, staff.last_name, payment.amount, payment.payment_date
 FROM staff INNER JOIN payment ON
 staff.staff_id = payment.staff_id AND payment_date LIKE '2005-08%'; 
 
--- 6c. 
 
 SELECT f.title AS 'Film Title', COUNT(fa.actor_id) AS `Number of Actors`
 FROM film_actor fa
@@ -97,7 +81,6 @@ INNER JOIN film f
 ON fa.film_id= f.film_id
 GROUP BY f.title;
 
--- 6d. 
 
 SELECT title, (
 SELECT COUNT(*) FROM inventory
@@ -106,7 +89,6 @@ WHERE film.film_id = inventory.film_id
 FROM film
 WHERE title = "Hunchback Impossible";
 
--- 6e. 
 
 SELECT c.first_name, c.last_name, sum(p.amount) AS `Total Paid`
 FROM customer c
@@ -114,7 +96,6 @@ JOIN payment p
 ON c.customer_id= p.customer_id
 GROUP BY c.last_name;
 
--- 7a.
 
 SELECT title
 FROM film WHERE title 
@@ -126,7 +107,6 @@ FROM film
 WHERE language_id = 1
 );
 
--- 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
 
 SELECT first_name, last_name
 FROM actor
@@ -141,7 +121,6 @@ FROM film
 WHERE title = 'Alone Trip'
 ));
 
--- 7c. 
 
 SELECT cus.first_name, cus.last_name, cus.email 
 FROM customer cus
@@ -153,7 +132,6 @@ JOIN country
 ON (country.country_id = cty.country_id)
 WHERE country.country= 'Canada';
 
--- * 7d. 
 
 SELECT title, description FROM film 
 WHERE film_id IN
@@ -165,7 +143,6 @@ SELECT category_id FROM category
 WHERE name = "Family"
 ));
 
--- 7e. 
 
 SELECT f.title, COUNT(rental_id) AS 'Times Rented'
 FROM rental r
@@ -176,7 +153,6 @@ ON (i.film_id = f.film_id)
 GROUP BY f.title
 ORDER BY `Times Rented` DESC;
 
--- 7f. 
 
 SELECT s.store_id, SUM(amount) AS 'Revenue'
 FROM payment p
@@ -188,7 +164,6 @@ JOIN store s
 ON (s.store_id = i.store_id)
 GROUP BY s.store_id; 
 
--- 7g. 
 
 SELECT s.store_id, cty.city, country.country 
 FROM store s
@@ -200,7 +175,6 @@ JOIN country
 ON (country.country_id = cty.country_id);
 
 
---  7h.
 
 SELECT c.name AS 'Genre', SUM(p.amount) AS 'Gross' 
 FROM category c
@@ -214,7 +188,6 @@ JOIN payment p
 ON (r.rental_id=p.rental_id)
 GROUP BY c.name ORDER BY Gross  LIMIT 5;
 
---  8a. 
 
 CREATE VIEW genre_revenue AS
 SELECT c.name AS 'Genre', SUM(p.amount) AS 'Gross' 
@@ -229,10 +202,8 @@ JOIN payment p
 ON (r.rental_id=p.rental_id)
 GROUP BY c.name ORDER BY Gross  LIMIT 5;
   	
---  8b. 
 
 SELECT * FROM genre_revenue;
 
---  8c. 
 
 DROP VIEW genre_revenue;
